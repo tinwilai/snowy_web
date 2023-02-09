dd9a5d6e79146d0b1176355fdbd6551c8284e4a9
Tin W <tinwilai@outlook.com>
2023-02-10 00:10:59 +0900

    Ignore avoid_web_libraries_in_flutter

--- lib/pages/home/location.dart
+++ lib/pages/home/location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// ignore: avoid_web_libraries_in_flutter
 import 'dart:html';
 import 'dart:ui' as ui;
 import 'package:snowyweb/global/global.dart' as global;
